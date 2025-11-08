b50a31233f8c4faaa6647cc536bb32e1ea8f6821
anaypurohit0907 <ap1851@srmist.edu.in>
2025-11-08 17:53:28 +0530

    added benchmarking and documentation

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 <img width="708" height="168" alt="image" src="https://github.com/user-attachments/assets/325e9d29-1f06-4766-adbc-30b6815f3473" />
+
+
+## Custom memory allocator
+
+# project purpose: 
+    the purpose of this project was to learn about memory managent, specifically memory allocation and truly understand how it works under the hood.
+
+# key features:
+    -helper functions to see working of the allocator: ``` bytes_used() , bytes_free(), largest_free_block() ``` 
+    -blocks coalescing : neighbouring free blocks are joined to form one singular block which allows the allocator to allocate larger memory sizes too.
+
+# implementation details:
+    -data structures: struct- to define and manage a block.
+                        doubly linked list- to manage the blocks in the free list
+    -
--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 #include <cassert>
 #include <chrono>
 #include <cstdio>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
@@ -11,33 +16,102 @@ constexpr std::size_t BUFFER_SIZE = 1024 * 1024 * 8; // 1MB
 alignas(std::max_align_t) char buffer[BUFFER_SIZE];
 constexpr int N = 100000;
 
+struct PoolDeleter {
+  void operator()(void *ptr) const {
+    mm::deallocate(ptr); // Return block to your pool
+  }
+};
+
+struct PoolDeleter_malloc {
+  void operator()(void *ptr) const {
+    free(ptr); // Return block to your pool
+  }
+};
+
 int main() {
   mm::init_pool(buffer, BUFFER_SIZE);
+
   std::vector<void *> ptrs(N);
 
+  std::vector<void *> ptrs_malloc(N);
+
   auto start = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i < N; ++i) {
-    ptrs[i] = mm::allocate(32);
-    assert(ptrs[i] != nullptr);
-  }
-  for (int i = 0; i < N; ++i) {
-    mm::deallocate(ptrs[i]);
+  try {
+    for (int i = 0; i < N; ++i) {
+      ptrs[i] = mm::allocate(32);
+      assert(ptrs[i] != nullptr);
+      //     if (i == N / 2)
+      //        throw std::runtime_error("simulated exception");
+    }
+    std::cout << "free block after allocation1: " << mm::bytes_free()
+              << std::endl;
+
+    for (int i = 0; i < N; i += 2) {
+      mm::deallocate(ptrs[i]);
+      ptrs[i] = nullptr;
+    }
+
+    std::cout << "free block after de-allocation: " << mm::bytes_free()
+              << std::endl;
+
+    std::cout << "largest free block after de-allocation: "
+              << mm::largest_free_block() << std::endl;
+
+    std::vector<void *> nptrs(N);
+    for (int i = 0; i < N; i += 2) {
+      nptrs[i] = mm::allocate(32);
+      assert(nptrs[i] != nullptr);
+    }
+
+    std::cout << "free block after allocation2: " << mm::bytes_free()
+              << std::endl;
+
+    std::cout << "largest free block after allocation2: "
+              << mm::largest_free_block() << std::endl;
+
+  } catch (const std::exception &e) {
+    printf("caugh exception %s\n", e.what());
   }
   auto end = std::chrono::high_resolution_clock::now();
+
+  /* auto check = mm::bytes_free();
+  auto bytes_used = mm::bytes_used();
+  printf("bytes free: %zu\n", check);
+
+  printf("bytes used: %zu\n", bytes_used);
+*/
   double custom_time = std::chrono::duration<double>(end - start).count();
   printf("Custom allocator: %.6f seconds\n", custom_time);
 
-  start = std::chrono::high_resolution_clock::now();
-  for (int i = 0; i < N; ++i) {
-    ptrs[i] = malloc(32);
-    assert(ptrs[i] != nullptr);
+  auto start1 = std::chrono::high_resolution_clock::now();
+  try {
+    for (int i = 0; i < N; ++i) {
+      ptrs[i] = malloc(32);
+      assert(ptrs[i] != nullptr);
+    }
+    std::cout << "malloc allocation 1 complete" << std::endl;
+
+    for (int i = 0; i < N; i += 2) {
+      free(ptrs[i]);
+      ptrs[i] = nullptr;
+    }
+
+    std::cout << "malloc de-allocation  complete" << std::endl;
+    std::vector<void *> nptrs(N);
+    for (int i = 0; i < N; i += 2) {
+      nptrs[i] = malloc(32);
+      assert(nptrs[i] != nullptr);
+    }
+
+    std::cout << "malloc allocation 2 complete" << std::endl;
+
+  } catch (const std::exception &e) {
+    printf("caugh exception %s\n", e.what());
   }
-  for (int i = 0; i < N; ++i) {
-    free(ptrs[i]);
-  }
-  end = std::chrono::high_resolution_clock::now();
-  double malloc_time = std::chrono::duration<double>(end - start).count();
-  printf("malloc/free:      %.6f seconds\n", malloc_time);
+  auto end1 = std::chrono::high_resolution_clock::now();
+
+  double malloc_time = std::chrono::duration<double>(end1 - start1).count();
+  printf("malloc: %.6f seconds\n", malloc_time);
 
   return 0;
 }
